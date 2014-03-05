0fa4875fc26ecdf4aa14db6529b6737a1cf8acf8
AWilli <AWilli@10.2.35.213>
2014-03-05 08:42:20 +0100

    3a)  mult() throws IllegalArgumentException(); 3b) fixed function f()

--- U01_BirmiGang/u1/u1a3/Mult.java
+++ U01_BirmiGang/u1/u1a3/Mult.java
@@ -2,13 +2,12 @@ package u1a3;
 
 public class Mult {
     private static int f(int a, int b) {
-        if (b==0) return a;
-        if (b%2 == 0) return f(2*a, b/2);
+        if (b==1) return a;  if (b%2 == 0) return f(2*a, b/2);
         else return a + f(2*a, b/2);
     }
     
     public static int mult(int a, int b) {
-
+        if (a < 1 || b < 1) throw new IllegalArgumentException();
         return f(a, b);
     }
 }
