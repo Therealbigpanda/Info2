c62460bed8eefdea7eefa8daa054e699fca7c460
AWilli <AWilli@10.2.35.213>
2014-03-05 08:47:46 +0100

    basic javadoc

--- U01_BirmiGang/u1/u1a3/Mult.java
+++ U01_BirmiGang/u1/u1a3/Mult.java
@@ -1,13 +1,24 @@
 package u1a3;
 
+/**
+ * @version 1.0
+ * @author Andrea-Pascal Willi & Andrea Tuccillo
+ */
 public class Mult {
     private static int f(int a, int b) {
         if (b==1) return a;  if (b%2 == 0) return f(2*a, b/2);
         else return a + f(2*a, b/2);
     }
     
+    /**
+     *
+     * @param a
+     * @param b
+     * @return f(a,b);
+     * @throws IllegalArgumentException "e"
+     */
     public static int mult(int a, int b) {
-        if (a < 1 || b < 1) throw new IllegalArgumentException();
+        if (a < 1 || b < 1) throw new IllegalArgumentException(e);
         return f(a, b);
     }
 }
