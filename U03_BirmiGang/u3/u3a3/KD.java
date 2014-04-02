049a3f0987885db797a67fa16a59a71ef4b624c3
AWilli <AWilli@AWilli-PC>
2014-04-02 22:47:03 +0200

    garbage + garbage2 ungelöst

--- U03_BirmiGang/u3/u3a3/KD.java
+++ U03_BirmiGang/u3/u3a3/KD.java
@@ -1,20 +1,16 @@
 package u3a3;
 
 /**
- *
- * @author AWilli
+ * @since a long time
+ * @author Andrea-Pascal Willi
  */
 public class KD {
 
     /**
-     *
+     * global string ref to access param of parse(String kd)
      */
     public static String tree;
 
-    /**
-     *
-     */
-    public int baumlänge = 0;
     /**
      * Parse a "Klammerdarstellung" (KD) of a tree.
      * 
@@ -24,28 +20,28 @@ public class KD {
      * <li>Children are appended to the father as a ','-separated list of nodes enclosed in round brackets.</li>
      * </ul> 
      * 
-     * @param kd Tree encoded in KD
+     * @param kd: tree encoded in KD
      * @throws ParseException if the given String is not a valid KD of a tree.
      */
     public static void parse(String kd) throws ParseException{
-        tree = kd;
-        if(tree.length() == 0) throw new ParseException("empyString", 0);
+        tree = kd; // initialize global string-ref to kd
+        if(tree.length() == 0) throw new ParseException("empty string", 0);
         if(tree.length() > 1 && tree.charAt(1) != '(') throw new ParseException("noRoot", 0);
-        tree(0); 
+        tree(0); // check if kd is a valid tree
     }
 
     /**
      *
      * @param index: where the tree is located in the string
-     * @return if tree has one node: "1"; if tree hast more nodes: "3 + next(index+2)"
+     * @return number of chars in tree (node + '(' + number of chars in subtree + ')')
      * @throws ParseException if there is an error in the tree format
      */
     public static int tree(int index) throws ParseException
     {
-        if(tree.charAt(index) == '-')
+        if(tree.charAt(index) == '-') // if tree is empty return 1
         {	
             if(tree.length() > index+1 && tree.charAt(index+1) == '(')
-                throw new ParseException("noRoot but children", 0);		
+                throw new ParseException("no root but children", 0);		
             return 1;
         }
         
@@ -75,7 +71,9 @@ public class KD {
         {
             subelements += tree(index+subelements+1) + 1;            
         }
-        
         return subelements;
     }
 }
+
+// A , B , C
+// 0 1 2 3 4
