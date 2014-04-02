40608582429870d7cd51373524ab16b1ed4a3ffa
AWilli <AWilli@AWilli-PC>
2014-04-02 15:53:07 +0200

    garbage + garbage2 ungelöst

--- U03_BirmiGang/u3/u3a3/KD.java
+++ U03_BirmiGang/u3/u3a3/KD.java
@@ -1,7 +1,19 @@
 package u3a3;
 
+/**
+ *
+ * @author AWilli
+ */
 public class KD {
+
+    /**
+     *
+     */
     public static String tree;
+
+    /**
+     *
+     */
     public int baumlänge = 0;
     /**
      * Parse a "Klammerdarstellung" (KD) of a tree.
@@ -17,40 +29,53 @@ public class KD {
      */
     public static void parse(String kd) throws ParseException{
         tree = kd;
-        if(tree.length() == 0) throw new ParseException("empty tree", 0);
-        if(tree.length() > 1 && tree.charAt(1) != '(') throw new ParseException("no root", 0);
-        baum(0);
+        if(tree.length() == 0) throw new ParseException("empyString", 0);
+        if(tree.length() > 1 && tree.charAt(1) != '(') throw new ParseException("noRoot", 0);
+        tree(0); 
     }
 
-    public static boolean baum(int index) throws ParseException{
+    /**
+     *
+     * @param index: where the tree is located in the string
+     * @return if tree has one node: "1"; if tree hast more nodes: "3 + next(index+2)"
+     * @throws ParseException if there is an error in the tree format
+     */
+    public static int tree(int index) throws ParseException
+    {
         if(tree.charAt(index) == '-')
-        {
+        {	
             if(tree.length() > index+1 && tree.charAt(index+1) == '(')
-                throw new ParseException("emptry tree can not have children", index);
-            return true;
+                throw new ParseException("noRoot but children", 0);		
+            return 1;
         }
         
-        if(!knoten(index)) throw new ParseException("invalid node", index);
-        if(tree.length() > index+1 && knoten(index) && knoten(index+1)) throw new ParseException("two nodes", index);
+        if(!Character.isUpperCase(tree.charAt(index))) throw new ParseException("illegal node", index);
         
-        if(tree.length() > index+1 && tree.charAt(index+1)=='(')
+        if(tree.length() > index+1 && tree.charAt(index+1) == '(')
         {
-            if(tree.length() < index+4) throw new ParseException("Tree ends unexpectedly", index+3);
-            nachfolger(index+2);
+            if(tree.length() < index+4) throw new ParseException("treeing ends abruptly", index);
+            int subelements = next(index+2);
+            if(tree.charAt(subelements+index+2) == ')')
+                return 3 + subelements;
+            else throw new ParseException("no closing braces", index);
         }
-        return false;
+        return 1;
     }
     
-    public static boolean knoten(int index) throws ParseException{
-        if (Character.isUpperCase(tree.charAt(index))) return true;
-        else return false;
-    }
-
-    public static void nachfolger(int index) throws ParseException{
-        baum(index);
-        if(tree.charAt(index+1) == ',')
+    /**
+     *
+     * @param index: where the subtree is located in the string
+     * @return
+     * @throws ParseException
+     */
+    public static int next(int index) throws ParseException
+    {
+        int subelements = tree(index);
+        while(tree.charAt(index+subelements) == ',')
         {
-            baum(index+2);
+            subelements += tree(index+subelements+1) + 1;            
         }
+        
+        return subelements;
     }
 }
