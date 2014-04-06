a6c53e77d49427429eddfcb837d17dd8d1b15809
AWilli <AWilli@AWilli-PC.home>
2014-04-07 01:24:34 +0200

    gelöst

--- U04_BirmiGang/u4/u4a2/IterativeAckermann.java
+++ U04_BirmiGang/u4/u4a2/IterativeAckermann.java
@@ -3,28 +3,28 @@ package u4a2;
 import u4a1.Stack;
 
 public class IterativeAckermann {
-	/**
-	 * Stack-based iterative implementation of the Ackermann function.
-	 * 
-	 * @param n parameter n
-	 * @param m parameter m
-	 * @return Ackermann(n,m)
-	 */
-	public int A(int n, int m) {
-            Stack stack = new Stack(1);
-            while(stack.size() > 0 || n != 0) {
-                if (n == 0) {
-                    ++m;
-                    n = stack.pop();
-                }
-                else if (m == 0) {
-                    --n;
-                    ++m;
-                } else {
-                    stack.push(n-1);
-                    --m;
-                }
+    /**
+     * Stack-based iterative implementation of the Ackermann function.
+     * 
+     * @param n parameter n
+     * @param m parameter m
+     * @return Ackermann(n,m)
+     */
+    public int A(int n, int m) {
+        Stack stack = new Stack(1);
+        while(stack.size() > 0 || n != 0) {
+            if (n == 0) {
+                ++m;
+                n = stack.pop();
             }
-            return ++m;
-	}
+            else if (m == 0) {
+                --n;
+                ++m;
+            } else {
+                stack.push(n-1);
+                --m;
+            }
+        }
+        return ++m;
+    }
 }
