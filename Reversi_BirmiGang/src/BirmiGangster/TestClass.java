636d9f9b4277eba48339b91d3bb5ac68797e5dcd
AWilli <AWilli@AWilli-PC.home>
2014-04-18 03:58:43 +0200

    TestClass angepasst; Test Mode hinzugefügt

--- Reversi_BirmiGang/src/BirmiGangster/TestClass.java
+++ Reversi_BirmiGang/src/BirmiGangster/TestClass.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package BirmiGangster;
 
 import java.io.File;
@@ -18,72 +12,80 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author AWilli
+ * @author Andrea-Pascal Willi & Andrea Tuccillo
  */
 public class TestClass {
-    
-    
-    
+
     public static void main(String[] args) {
-        
-        
-        File file = new File("/Users/andreatuccillo/NetBeansProjects/Info2/Reversi_BirmiGang/reversilog");
- 
-         if(file.delete()){
-             System.out.println(file.getName() + " is deleted!");
-        }else{
+
+        File file = new File("reversilog");
+        String filepath = file.getAbsolutePath();
+
+        if (file.delete()) {
+            System.out.println(file.getName() + " is deleted!");
+        } else {
             System.out.println("Delete operation is failed.");
         }
-        
-        for (int i = 0; i < 20; i++) { 
+
+        for (int i = 0; i < 20; i++) {
             try {
                 reversi.Arena.main(args);
-            } catch(NullPointerException e) {
+            } catch (NullPointerException e) {
                 //do nothing
             }
         }
-        Path path = FileSystems.getDefault().getPath("/Users/andreatuccillo/NetBeansProjects/Info2/Reversi_BirmiGang/reversilog");
+        Path path = FileSystems.getDefault().getPath(filepath);
         List<String> stringList;
-        
+
         int redWins = 0;
         int greenWins = 0;
-        
+
         int draw = 0;
-        
+
         int reds = 0;
         int greens = 0;
         try {
             stringList = Files.readAllLines(path, UTF_8);
-            for(String line: stringList){
-                if(line.contains("finished")){
-                    int firstRedDigit = Integer.parseInt(line.substring(line.indexOf("reds=")+5, line.indexOf("reds=")+6));
-                    
-                    try{
-                        int secondRedDigit = Integer.parseInt(line.substring(line.indexOf("reds=")+6, line.indexOf("reds=")+7));
-                        reds = 10*firstRedDigit + secondRedDigit;
-                    }catch (java.lang.NumberFormatException e){
+            for (String line : stringList) {
+                if (line.contains("finished")) {
+                    int firstRedDigit = Integer.parseInt(line.substring(line.indexOf("reds=") + 5, line.indexOf("reds=") + 6));
+
+                    try {
+                        int secondRedDigit = Integer.parseInt(line.substring(line.indexOf("reds=") + 6, line.indexOf("reds=") + 7));
+                        reds = 10 * firstRedDigit + secondRedDigit;
+                    } catch (java.lang.NumberFormatException e) {
                         reds = firstRedDigit;
+                    } catch (StringIndexOutOfBoundsException x) {
+                        //do nothing
                     }
-                    
-                    int firstGreenDigit = Integer.parseInt(line.substring(line.indexOf("greens=")+7, line.indexOf("greens=")+8));
-                    
-                    try{
-                        int secondGreenDigit = Integer.parseInt(line.substring(line.indexOf("greens=")+8, line.indexOf("greens=")+9));
-                        reds = 10*firstGreenDigit + secondGreenDigit;
-                    }catch (java.lang.NumberFormatException e){
+
+                    int firstGreenDigit = Integer.parseInt(line.substring(line.indexOf("greens=") + 7, line.indexOf("greens=") + 8));
+
+                    try {
+                        int secondGreenDigit = Integer.parseInt(line.substring(line.indexOf("greens=") + 8, line.indexOf("greens=") + 9));
+                        greens = 10 * firstGreenDigit + secondGreenDigit;
+                    } catch (java.lang.NumberFormatException e) {
                         greens = firstGreenDigit;
+                    } catch (StringIndexOutOfBoundsException x) {
+                        // do nothing
                     }
-                    
-                    if(reds > greens) redWins++;
-                    if(reds == greens) draw ++;
-                    if(reds < greens) greenWins ++;
+
+                    if (reds > greens) {
+                        redWins++;
+                    }
+                    if (reds == greens) {
+                        draw++;
+                    }
+                    if (reds < greens) {
+                        greenWins++;
+                    }
                 }
             }
         } catch (IOException ex) {
             Logger.getLogger(TestClass.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        System.out.println("Red Wins: "+redWins+" Green Wins: "+greenWins+" Draws: "+draw);
+
+        System.out.println("Red Wins: " + redWins + " Green Wins: " + greenWins + " Draws: " + draw);
     }
-    
+
 }
