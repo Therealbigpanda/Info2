fa005ee535a6844f5f2355ec5817a8ad9926a5b8
AWilli <AWilli@AWilli-PC.home>
2014-04-18 04:06:45 +0200

    RandomPlayer aus U07

--- Reversi_BirmiGang/src/BirmiGangster/RandomPlayer.java
+++ Reversi_BirmiGang/src/BirmiGangster/RandomPlayer.java
@@ -15,7 +15,8 @@ public class RandomPlayer implements ReversiPlayer {
 
     private int myColor;
     private long timeLimit;
-    
+    Random randomInt = new Random();
+
     @Override
     public void initialize(int myColor, long timeLimit) {
         this.myColor = myColor;
@@ -24,32 +25,24 @@ public class RandomPlayer implements ReversiPlayer {
 
     @Override
     public Coordinates nextMove(GameBoard gb) {
-        Coordinates nextMove = null;
-        
-        Stack possibleMoves = possibleMoves(gb);
-        Random randomInt = new Random();
+        Stack<Coordinates> possibleMoves = possibleMoves(gb);
         if (possibleMoves.size() > 0) {
-            int b = randomInt.nextInt(possibleMoves.size());
-            for (int i = 0; i <= b; ++i) {
-                nextMove = (Coordinates)possibleMoves.pop();
-            }
-            return nextMove;
-        } else {
-            return null;
+            return possibleMoves.get(randomInt.nextInt(possibleMoves.size()));
         }
-            
-    }   
+        return null;
+    }
 
-    private Stack possibleMoves(GameBoard gb) {
+    private Stack<Coordinates> possibleMoves(GameBoard gb) {
         Stack possibleMoves = new Stack();
         Coordinates coords;
-        
-        for(int x = 1; x <= gb.getSize(); ++x) {
-            for(int y = 1; y <= gb.getSize(); ++y) {
-                coords = new Coordinates(x, y);
-                if(gb.checkMove(myColor, coords)) possibleMoves.push(coords);
+
+        for (int x = 1; x <= gb.getSize(); ++x) {
+            for (int y = 1; y <= gb.getSize(); ++y) {
+                if (gb.checkMove(myColor, coords = new Coordinates(x, y))) {
+                    possibleMoves.push(coords);
+                }
             }
-        }   
-        return possibleMoves; 
+        }
+        return possibleMoves;
     }
 }
