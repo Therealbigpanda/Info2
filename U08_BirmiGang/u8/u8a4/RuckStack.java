c3b9ca4ca8223fc9989bef670441ac1835a018a3
AWilli <AWilli@AWilli-PC.home>
2014-04-24 16:44:22 +0200

    hinzugefügt

---
+++ U08_BirmiGang/u8/u8a4/RuckStack.java
@@ -0,0 +1,16 @@
+package u8a4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Andrea-Pascal Willi
+ */
+public class RuckStack implements IRucksack {
+
+    @Override
+    public Selection findBest(ArrayList<Integer> values, ArrayList<Integer> weights, int maxWeight) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
