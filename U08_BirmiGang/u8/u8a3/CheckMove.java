d2f162fc237fe716d5fe5a3f032527f6eb1633b8
AWilli <AWilli@AWilli-PC.home>
2014-04-30 19:14:03 +0200

    gelöst

--- U08_BirmiGang/u8/u8a3/CheckMove.java
+++ U08_BirmiGang/u8/u8a3/CheckMove.java
@@ -11,66 +11,38 @@ import reversi.OutOfBoundsException;
  */
 public class CheckMove implements ICheckMove {
 
-    private final Coordinates[] allDirections = new Coordinates[8];
-    int player = 0;
-    GameBoard gb;
-
-    public CheckMove() {
-        allDirections[0] = new Coordinates(-1, -1);
-        allDirections[1] = new Coordinates(-1, 0);
-        allDirections[2] = new Coordinates(-1, 1);
-        allDirections[3] = new Coordinates(0, -1);
-        allDirections[4] = new Coordinates(0, 1);
-        allDirections[5] = new Coordinates(1, -1);
-        allDirections[6] = new Coordinates(1, 0);
-        allDirections[7] = new Coordinates(1, 1);
-    }
-
     @Override
     public boolean checkMove(GameBoard gb, int player, Coordinates coord) {
-        this.player = player;
-        this.gb = gb;
-
         try {
-            if (gb.getOccupation(coord) != EMPTY) {
+            if (gb.getOccupation(coord) != EMPTY)
                 return false;
-            }
         } catch (OutOfBoundsException e) {
             return false;
         }
-        for (Coordinates i : allDirections) {
-            if (isLineTurnable(coord, i)) {
-                return true;
-            }
-        }
+            
+        for (int i = -1; i <= 1; ++i)
+            for (int j = -1; j <= 1; ++j)
+                if (i != 0 || j != 0)
+                    if (checkDirection(gb, player, coord, i, j)) 
+                        return true; 
         return false;
     }
 
-    private boolean isLineTurnable(Coordinates coord, Coordinates direction) {
-        boolean hasEnemyStonesInLine = false;
-
-        Coordinates nextCoord = nextCoord(coord, direction);
-
+    private boolean checkDirection(GameBoard gb, int player, Coordinates coord, int x, int y) {
         try {
-            while (gb.getOccupation(nextCoord) != player) {
-                if (!gb.validCoordinates(nextCoord)) {
-                    return false;
-                }
-                if (gb.getOccupation(nextCoord) == EMPTY) {
-                    return false;
-                } else {
-                    hasEnemyStonesInLine = true;
-                    nextCoord = nextCoord(nextCoord, direction);
-                }
+            Coordinates newCoord = new Coordinates(coord.getRow() + x, coord.getCol() + y);
+            if (gb.getOccupation(newCoord) == EMPTY) {
+                return false;
+            }
+            if (gb.getOccupation(coord) == reversi.Utils.other(player) && gb.getOccupation(newCoord) == player) {
+                return true;
+            }
+            if (gb.getOccupation(newCoord) == reversi.Utils.other(player)) {
+                return checkDirection(gb, player, newCoord, x, y);
             }
         } catch (OutOfBoundsException e) {
             return false;
         }
-
-        return hasEnemyStonesInLine;
-    }
-
-    private Coordinates nextCoord(Coordinates coord, Coordinates direction) {
-        return new Coordinates(coord.getRow() + direction.getRow(), coord.getCol() + direction.getCol());
+        return false;
     }
 }
